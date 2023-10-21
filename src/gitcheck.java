df9f97a304b6ffc5cebe3478238388115efc2556
Shreyas-Test1 <shreyaspoojary321@gmail.com>
2023-10-21 18:12:10 +0530

    first commit

---
+++ src/gitcheck.java
@@ -0,0 +1,17 @@
+
+public class gitcheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+		
+		System.out.println("Check if file is updated");
+		
+		
+	}
+
+}
